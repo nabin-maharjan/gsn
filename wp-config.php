ce3bebfe551ed31ba7fce4e81e8b350ed9f778be
chimseryOo001 <chimsemgr001@gmail.com>
2016-12-22 21:28:11 +0545

    Add new template for client template and required folders for styles

--- wp-config.php
+++ wp-config.php
@@ -80,12 +80,12 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-// ini_set('log_errors','On');
-// ini_set('display_errors','Off');
-// ini_set('error_reporting', E_ALL );
+ini_set('log_errors','On');
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
 define('WP_DEBUG', false);
-// define('WP_DEBUG_LOG', true);
-// define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
 
 /* That's all, stop editing! Happy blogging. */
 
---
+++ wp-content/themes/gsn/page-templates/client-theme.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template Name: Client Theme
+ *
+ * @package GSN
+ * @since GSN 1.0
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Client Theme</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.min.css">
+</head>
+<body>
+  <header class="header gsn-header">
+    <div class="header__top">
+      <div class="container">
+        <div class="row">
+          <div class="top top-icons clearfix p15">
+            <div class="top__left-icons fl">
+              <ul>
+                <li><i class="fa fa-phone"></i><a href="tel:014444444">014444444</a></li>
+                <li><i class="fa fa-map-marker"></i>Somwhere</li>
+              </ul>
+            </div>
+            <!-- /.top__left-icons -->
+            <div class="top__right-cons fr">
+              <ul>
+                <li><i class="fa fa-profile"></i><a href="#">Sign In</a></li>
+                <li></li>
+              </ul>
+            </div>
+            <!-- /.top__right-icons -->
+          </div>
+          <!-- /.top-icons -->
+        </div>
+      </div>
+    </div>
+    <!-- /.header__top -->
+    <div class="header__bottom">
+      
+    </div>
+    <!-- /.header__bottom -->
+  </header>
+  <!-- /.gsn-header -->
+</body>
+</html>
