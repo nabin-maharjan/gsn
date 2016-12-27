5e56d2114923b62a65df2fb40b59abf3a72e05a6
chimseryOo001 <chimsemgr001@gmail.com>
2016-12-27 21:00:28 +0545

    Footer final pieces added

--- wp-content/themes/gsn/footer-store.php
+++ wp-content/themes/gsn/footer-store.php
@@ -6,26 +6,44 @@
           <h3>Keep in touch</h3>
           <ul class="contact-info">
             <li>12345 Street name, California, USA</li>
-            <li>0123 456 789 / 0123 456 788</li>
-            <li>support@company.com</li>
+            <li><a href="tel:0123 456 789">0123 456 789</a></li>
+            <li><a href="mailto:support@company.com">support@company.com</a></li>
           </ul>
         </div>
         <!-- /.footer__contact -->
         <div class="col-sm-6 footer__items footer-map">
           <h3>Map</h3>
-          <div class="footer-map-cntr">
+          <div class="footer-map-cntr" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/new-1.jpg');">
             
           </div>
         </div>
         <!-- /.footer__map -->
         <div class="col-sm-3 footer__items footer-social">
           <h3>Social links</h3>
+          <ul>
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+          </ul>
         </div>
         <!-- /.footer__social -->
       </div>
     </div>
   </div>
   <!-- /.top-footer -->
+  <div class="bottom-footer">
+    <div class="container">
+      <div class="bottom-links">
+        <ul class="footer-links clearfix">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Category</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!-- /.bottom-footer -->
 </footer>
   <!-- /.footer -->
 <?php wp_footer(); ?>
