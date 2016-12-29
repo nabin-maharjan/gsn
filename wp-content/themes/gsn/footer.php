d48342103527a78763871ad91a1b6568216bfa0f
nabin <nabin.maharjan@agileitsolutions.net>
2016-12-29 07:39:33 +0545

    add about page

--- wp-content/themes/gsn/footer.php
+++ wp-content/themes/gsn/footer.php
@@ -1,9 +1,10 @@
-<?php if(is_page() || is_archive() || is_tax()){
+<?php 
+$footer_name="";
+if(is_page() || is_archive() || is_tax()){
 	global $post;
 	$store_page=get_post_meta($post->ID,'store_page',true);
 	if((!empty($store_page) && $store_page=="yes") || is_archive()|| is_tax()){
-		get_template_part( 'template-parts/footer/footer', 'store' ); 
-	}else{
-		get_template_part( 'template-parts/footer/footer', '' ); 
+		$footer_name="store";
 	}
 }
+get_template_part( 'template-parts/footer/footer',$footer_name); 
