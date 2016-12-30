741a9180d2bdb0c0b0d414a0f3d2516c4e7cf641
Aakash Thapa <aakash.thapa@agileitsolutions.net>
2016-12-30 08:01:01 +0545

    Dashboard landing design

--- wp-content/themes/gsn/template-parts/dashboard/content.php
+++ wp-content/themes/gsn/template-parts/dashboard/content.php
@@ -1,3 +1,23 @@
-<section>
-This is dashboard.
+<section class="dashboard-landing-cntr">
+  <div class="container">
+    <div class="row">
+      <div class="dashboard-top">
+        <h1 class="dashboard-title">Summary of your store.</h1>
+      </div>
+      <!-- /.dashboard-top -->
+      <div class="summary-cards-cntr clearfix">
+        <div class="col-sm-4 summary__card">
+          <a href="#" class="summary-info">
+            <div class="">
+              <span class="summary-number">10</span>
+              <h2>Title</h2>
+            </div>
+          </a>
+        </div>
+        <!-- /.summary__card -->
+      </div>
+      <!-- /.dashboard-summary-cntr -->
+    </div>
+  </div>
 </section>
+<!-- /.dashboard-landing-cntr -->
