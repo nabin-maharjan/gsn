4b081570cb8d524dad8553b22324b680ffdfebf8
nabin <nabin.maharjan@agileitsolutions.net>
2017-01-23 08:19:01 +0545

    add ad on category page

--- wp-content/themes/gsn/woocommerce/content-category.php
+++ wp-content/themes/gsn/woocommerce/content-category.php
@@ -23,6 +23,28 @@ global $store;
 global $gsnSettingsClass;
 $gsn_themes=$gsnSettingsClass->available_theme();
 $gsn_settings=$gsnSettingsClass->get();
+
+
+//// Middle section Left ad
+$middle_section_left_ad=get_option("home_page_middle_section_left_ad");
+$middle_section_left_ad_link="";
+$middle_section_left_ad_flag=false;
+if(!empty($middle_section_left_ad)){
+		$middle_section_left_ad_url=wp_get_attachment_url($middle_section_left_ad);
+		$middle_section_left_ad_link=get_option("home_page_middle_section_left_ad_link");
+		$middle_section_left_ad_flag=true;
+}
+
+
+//// Middle section Right ad
+$middle_section_right_ad=get_option("home_page_middle_section_right_ad");
+$middle_section_right_ad_link="";
+$middle_section_right_ad_flag=false;
+if(!empty($middle_section_right_ad)){
+		$middle_section_right_ad_url=wp_get_attachment_url($middle_section_right_ad);
+		$middle_section_right_ad_link=get_option("home_page_middle_section_right_ad_link");
+		$middle_section_right_ad_flag=true;
+}
 ?>
 
 <main class="main main-content">
@@ -63,9 +85,15 @@ $gsn_settings=$gsnSettingsClass->get();
 		</div>
 	</section>
 	<!-- /.page-hero -->
+    
+    
+     
+ 
+    
+    
+    
 	<section class="item-list-cntr">
 		<div class="container">
-			<div class="row">
 				<div class="list__items">
 					<ul>
                     <?php 
@@ -82,14 +110,29 @@ $gsn_settings=$gsnSettingsClass->get();
 							$child_categories = get_categories($args );
 						foreach($child_categories as $child){
 					?>
-						<li class="col-sm-2 col-md-3 list-item"><a href="<?php echo get_term_link($child->term_id);?>"><?php echo $child->name;?></a></li>
+						<li class="list-item"><a href="<?php echo get_term_link($child->term_id);?>"><?php echo $child->name;?></a></li>
                         <?php } ?>
-
 					</ul>
 				</div>
-			</div>
 		</div>
 	</section>
 	<!-- /.item-list-cntr -->
+    
+    <!-- Ad Container -->
+<?php if($middle_section_left_ad_flag || $middle_section_right_ad_flag){ ?>
+    <section class="middle_ad_cntr">
+    	<div class="container">
+            <div class="row">
+            <?php if($middle_section_left_ad_flag){ ?>
+                <div class="col-sm-6"><a <?php if($middle_section_left_ad_link){ ?> href="<?php echo $middle_section_left_ad_link;?>" <?php } ?> class="home-mid-ad" style="background-image:url('<?php echo  $middle_section_left_ad_url; ?>');"></a></div>
+             <?php } ?>   
+               <?php if($middle_section_right_ad_flag){ ?>
+                <div class="col-sm-6"><a <?php if($middle_section_right_ad_link){ ?> href="<?php echo $middle_section_right_ad_link;?>" <?php } ?> class="home-mid-ad" style="background-image:url('<?php echo  $middle_section_right_ad_url; ?>');"></a></div>
+             <?php } ?> 
+            </div>
+ 	   </div>
+    </section>
+ <?php } ?>  
+    <!-- / Ad Container -->
 </main>
 <!-- /.main -->
