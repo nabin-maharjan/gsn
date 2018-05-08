447281d015d2a8609127e76febfb33a925baa03c
Aakash Thapa <thapa.aakash100@gmail.com>
2018-05-08 12:36:49 +0545

    Added separate admin JS files and folder

--- wp-content/themes/gsn/page-templates/home-page.php
+++ wp-content/themes/gsn/page-templates/home-page.php
@@ -8,13 +8,16 @@
 get_header();
 ?>
 
-<main class="gsn-landing__main gsn-lmain">
-  <div class="container">
-    <div class="gsn-lmain__wrapper">
-      <div class="ta-c gsn-lmain__cta__wrapper">
-
+<main class="gsn-landing__main gsn-lmain">  
+  <div class="gsn-lmain__wrapper">
+    <section class="ta-c gsn-lmain__cta__wrapper gsn-lmcta">
+      <div class="container">
+        <div class="gsn-lmcta__heading">
+          <h1>Create your own free <br> <span>Ecommerce Website</span></h1>
+        </div>
       </div>
-    </div>
+    </section>
+    
   </div>
 </main>
 
